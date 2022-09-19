112c38cb87ce312d33fe90caab226bde68888f63
Nathan764 <nathanoli330@gmail.com>
2022-09-19 15:05:29 -0300

    calculador-de-IMC

--- script.js
+++ script.js
@@ -11,7 +11,18 @@ function calcular(){
     else if(soma >= 18.6 && soma <= 24.9){
         saida.innerHTML += `<p>Seu teste deu ${soma}. Peso ideal (Parabens)`
     }
-    else if (soma >= 25 && soma <= 29.9)
+    else if (soma >= 25 && soma <= 29.9) {
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Levemente acima do peso`
+    }
+    else if (soma >= 30 && soma <= 34.9){
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Obesidade Grau I`
+    }
+    else if (soma >= 35 && soma <= 39.9){
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Obesidade Grau II`
+    }
+    else {
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Obesidade Grau III`
+    }
 }
 
 
